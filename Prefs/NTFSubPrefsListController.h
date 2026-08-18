576ef9abaccfb29d198747658cbc9854df9d7c15
qwer12345uui <7.8293139e+07+qwer12345uui@users.noreply.github.com>
2026-08-18 12:10:51 +0000

    Use Cephei 2 appearance settings header

--- Prefs/NTFSubPrefsListController.h
+++ Prefs/NTFSubPrefsListController.h
@@ -1,7 +1,7 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 #import <CepheiPrefs/HBListController.h>
-#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <CepheiPrefs/CepheiPrefs.h>
 
 @interface NTFSubPrefsListController : HBListController
 
--- Prefs/Preferences.h
+++ Prefs/Preferences.h
@@ -1,7 +1,7 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 #import <CepheiPrefs/HBRootListController.h>
-#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <CepheiPrefs/CepheiPrefs.h>
 #import <Cephei/HBPreferences.h>
 #import "NSTask.h"
 
